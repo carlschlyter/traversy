a308dde63bb35a33a45d997a5943852bc93423d9
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2019-03-23 16:06:46 +0100

    Initiated the class person and instantiated the object person1

---
+++ oop.php
@@ -0,0 +1,14 @@
+<?php
+
+    class person{
+        public $name;
+        public $email;
+    }
+
+    $person1 = new person;
+
+    $person1->name = 'Barbra Streisand';
+
+    echo $person1->name;
+
+?>
